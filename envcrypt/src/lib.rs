44195d94e209f720e18970961fe3ef1355a20e7c
Cass Fridkin <cass@cloudflare.com>
2022-09-01 10:44:50 -0500

    fix docs??

--- envcrypt/src/lib.rs
+++ envcrypt/src/lib.rs
@@ -1,4 +1,53 @@
-#![cfg_attr(not(doctest), doc = include_str!("../../README.md"))]
+//! # envcrypt
+//! Drop-in replacement for [`env!`](https://doc.rust-lang.org/std/macro.env.html)
+//! that encrypts your variables at compile-time and decrypts them at runtime,
+//! preventing naughty folks from snooping your binary for secrets or credentials.
+//!
+//! ## Usage
+//!
+//! ```ignore
+//! use envcrypt::envcrypt;
+//!
+//! fn main() {
+//!   let my_super_secret_key = envcrypt!("SECRET_KEY");
+//!   // do stuff with your secret key
+//! }
+//! ```
+//!
+//! With [`dotenv`](https://crates.io/crates/dotenv):
+//!
+//! `.env`:
+//!
+//! ```dotenv
+//! CLIENT_SECRET="my_client_secret"
+//! SOME_TOKEN="some_token"
+//! ```
+//!
+//! `build.rs`:
+//!
+//! ```ignore
+//! fn main() {
+//!   println!("cargo:rerun-if-changed=.env");
+//!
+//!   for (key, value) in dotenv::vars() {
+//!     println!("cargo:rustc-env=${key}=${value}");
+//!   }
+//! }
+//! ```
+//!
+//! `main.rs`:
+//!
+//! ```ignore
+//! use envcrypt::envcrypt;
+//!
+//! fn main() {
+//!   let client_secret = envcrypt!("CLIENT_SECRET");
+//! }
+//! ```
+//!
+//! ## Details
+//!
+//! Encryption is powered by [`MagicCrypt`](https://crates.io/crates/magic-crypt) using AES-256 encryption.
 
 #[doc(hidden)]
 pub mod __internal {
