dc7d7a3c631e42a07c565a199d208fa27a537b54
Cass Fridkin <cass@cloudflare.com>
2022-09-02 16:54:51 -0500

    Clean up tests a little

--- envcrypt-test/tests/check_binary.rs
+++ envcrypt-test/tests/check_binary.rs
@@ -1,18 +1,46 @@
-use std::process::Command;
+use std::{
+    io,
+    process::{Command, Output},
+};
+
+fn run_binary() -> io::Result<Output> {
+    Command::new(env!("CARGO_BIN_EXE_envcrypt-test"))
+        .env("RUST_BACKTRACE", "1")
+        .output()
+}
+
+macro_rules! binary_bytes {
+    () => {
+        include_bytes!(env!("CARGO_BIN_EXE_envcrypt-test"))
+    };
+}
 
 #[test]
 fn check_binary() {
-    let stdout = Command::new(env!("CARGO_BIN_EXE_envcrypt-test"))
-        .output()
-        .expect("Failed to execute test binary!")
-        .stdout;
-    let stdout_text = String::from_utf8_lossy(&stdout);
+    let Output {
+        status,
+        stderr,
+        stdout,
+    } = run_binary().expect("Failed to execute test binary!");
 
-    assert!(stdout_text.contains("ENCRYPTED_VALUE"));
-    assert!(stdout_text.contains("NAKED_VALUE"));
+    let stderr = String::from_utf8_lossy(&stderr);
+    let stdout = String::from_utf8_lossy(&stdout);
 
-    let binary = include_bytes!(env!("CARGO_BIN_EXE_envcrypt-test"));
-    let binary_text = String::from_utf8_lossy(binary);
+    assert!(
+        status.success(),
+        "Test binary failed.\nstderr dump:\n{stderr}"
+    );
+
+    assert!(
+        stdout.contains("ENCRYPTED_VALUE"),
+        "Test binary failed.\nstderr dump:\n{stderr}"
+    );
+    assert!(
+        stdout.contains("NAKED_VALUE"),
+        "Test binary failed.\nstderr dump:\n{stderr}"
+    );
+
+    let binary_text = String::from_utf8_lossy(binary_bytes!());
 
     assert!(!binary_text.contains("ENCRYPTED_VALUE"));
     assert!(binary_text.contains("NAKED_VALUE"));
